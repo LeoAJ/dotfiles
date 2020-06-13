4ba3fe8a8817995ebc5ac69053f4b2ca67f3301a
LeoAJ <leo0924@gmail.com>
2020-06-13 19:49:47 +0800

    switch to oh my zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,45 @@
+unalias l
+
+# git
+alias gf="g fetch"
+alias gc="g clone"
+alias gl="g log"
+alias gst="g status"
+alias gaa="g add ."
+alias ggp="gp origin"
+alias ga="g add"
+alias gd="g diff"
+alias gstf="gst | fpp"
+alias glp="gl --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
+alias glol="gl --oneline"
+
+# docker
+alias dk="docker"
+alias dkc="dk container"
+alias dki="dk image"
+alias dc="docker-compose"
+
+# k8s
+alias k="kubectl"
+
+# rust
+alias cg="cargo"
+
+# node
+alias y="yarn"
+
+# others
+alias f="google"
+alias s="cd .."
+alias c="clear"
+alias h="history"
+alias t="tree"
+alias n="nvim"
+alias l="ls -1A"
+alias ll="ls -lh"
+alias b="brew"
+
+alias \
+	cp="cp -iv" \
+	mv="mv -iv" \
+	mkd="mkdir -pv"
---
+++ zsh/tool.zsh
@@ -0,0 +1,9 @@
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+# This is for `z` command
+# Move next only if `homebrew` is installed
+if command -v brew >/dev/null 2>&1; then
+	# Load rupa's z if installed
+	[ -f $(brew --prefix)/etc/profile.d/z.sh ] && source $(brew --prefix)/etc/profile.d/z.sh
+fi
