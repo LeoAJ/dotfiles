3fd4f020b332b474b1c3b7828c7c79ae3ee39713
LeoAJ <leo0924@gmail.com>
2020-06-24 08:46:29 +0800

    update prompt

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,6 +1,8 @@
-PROMPT="%B%{$fg[blue]%}[%{$fg[white]%}%\leo%{$fg[red]%}@%{$fg[white]%}%\code%{$fg[blue]%}]%{$reset_color%}%  %(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
-PROMPT+='%{$fg[cyan]%}%c%{$reset_color%} '
-PROMPT+='$(git_prompt_info)'
+# PROMPT="%B%{$fg[blue]%}[%{$fg[white]%}%\leo%{$fg[red]%}@%{$fg[white]%}%\code%{$fg[blue]%}]%{$reset_color%}%  %(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
+# PROMPT+='%{$fg[cyan]%}%c%{$reset_color%} '
+# PROMPT+='$(git_prompt_info)'
+
+PROMPT="%B%{$fg[red]%}[%{$fg[yellow]%}%\leo%{$fg[green]%}@%{$fg[blue]%}%\code %{$fg[magenta]%}%c%{$fg[red]%}]%{$reset_color%}% %(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}(%{$fg[red]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
