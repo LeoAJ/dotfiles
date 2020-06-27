02e7588019b29a4358168802d08d1347bc0f3e8d
LeoAJ <leo0924@gmail.com>
2020-06-27 15:21:24 +0800

    update zsh

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,11 +1,8 @@
-# PROMPT="%B%{$fg[blue]%}[%{$fg[white]%}%\leo%{$fg[red]%}@%{$fg[white]%}%\code%{$fg[blue]%}]%{$reset_color%}%  %(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
-# PROMPT+='%{$fg[cyan]%}%c%{$reset_color%} '
-# PROMPT+='$(git_prompt_info)'
-
-PROMPT="%B%{$fg[red]%}[%{$fg[yellow]%}%\leo%{$fg[green]%}@%{$fg[blue]%}%\code %{$fg[magenta]%}%c%{$fg[red]%}]%{$reset_color%}% %(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
+PROMPT="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%\code %{$fg[magenta]%}%c%{$fg[red]%}]%{$reset_color%} ➜%b "
+PROMPT+='$(git_prompt_info)'
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}(%{$fg[red]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%})"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
 
--- .config/zsh/tool.zsh
+++ .config/zsh/tool.zsh
@@ -1,3 +1,4 @@
+# for `nvm`
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
@@ -7,3 +8,5 @@ if command -v brew >/dev/null 2>&1; then
 	# Load rupa's z if installed
 	[ -f $(brew --prefix)/etc/profile.d/z.sh ] && source $(brew --prefix)/etc/profile.d/z.sh
 fi
+
+# for `deno`
