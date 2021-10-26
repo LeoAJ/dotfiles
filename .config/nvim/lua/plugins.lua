945a362abd4e9238a775a2f22e520b280e59620a
Leo Hsieh <leo0924@protonmail.com>
2021-10-25 23:34:59 -0700

    [major] init with lua ~

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,43 @@
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/nvim-lsp-installer'
+
+  -- theme
+  use 'gruvbox-community/gruvbox'
+
+  -- comment
+  use 'terrortylor/nvim-comment'
+
+  -- autopair
+  use 'windwp/nvim-autopairs'
+
+  -- treesitter
+  use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate"}
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
+  use 'jose-elias-alvarez/nvim-lsp-ts-utils'
+
+  -- telescope
+  use 'nvim-lua/popup.nvim'
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use 'nvim-telescope/telescope-fzy-native.nvim'
+
+  -- Completion
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+
+  use 'onsails/lspkind-nvim'
+
+  use {'folke/which-key.nvim'}
+end)
