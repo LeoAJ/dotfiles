f5bd4319e63e13333b3be6dc3bab0de905dbacff
Leo Hsieh <leo0924@protonmail.com>
2022-10-27 05:51:05 +0800

    update prompt

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -10,8 +10,8 @@ setopt PROMPT_SUBST
 # zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:git:*' formats " %{$fg[blue]%}(%{$fg[red]%}%m%u%c%{$fg[yellow]%}%{$fg[magenta]%} %b%{$fg[blue]%})"
 # 
-# format our main prompt for hostname current folder, and permissions.
-PROMPT="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%\me %{$fg[magenta]%}%c%{$fg[red]%}]%{$reset_color%} ➜%b "
+# PROMPT="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%\me %{$fg[magenta]%}%c%{$fg[red]%}]%{$reset_color%} ➜%b "
+PROMPT="🖥  %{$fg[magenta]%}%c%{$fg[red]%}%{$reset_color%} ➜%b"
 # PROMPT="%{$fg[green]%}%n@%m %~ %{$reset_color%}%#> "
 PROMPT+="\$vcs_info_msg_0_ "
 # PROMPT+='$(git_prompt_info)'
