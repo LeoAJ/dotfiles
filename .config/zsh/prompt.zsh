b6e2148f7c34a9dca891af65230438ca621fe652
Leo Hsieh <leo0924@protonmail.com>
2022-10-27 19:50:30 +0800

    update

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -11,7 +11,7 @@ setopt PROMPT_SUBST
 zstyle ':vcs_info:git:*' formats " %{$fg[blue]%}(%{$fg[red]%}%m%u%c%{$fg[yellow]%}%{$fg[magenta]%} %b%{$fg[blue]%})"
 # 
 # PROMPT="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%\me %{$fg[magenta]%}%c%{$fg[red]%}]%{$reset_color%} ➜%b "
-PROMPT="🖥  %{$fg[magenta]%}%c%{$fg[red]%}%{$reset_color%} ➜%b"
+PROMPT="🖥  %{$fg[magenta]%}%c%{$fg[red]%}%{$reset_color%} ➜%b "
 # PROMPT="%{$fg[green]%}%n@%m %~ %{$reset_color%}%#> "
 PROMPT+="\$vcs_info_msg_0_ "
 # PROMPT+='$(git_prompt_info)'
