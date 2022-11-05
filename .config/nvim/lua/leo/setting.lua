74a61b61fb7c598e8744f4c1d878b0351de233aa
Leo Hsieh <leo0924@protonmail.com>
2022-11-05 20:55:14 +0800

    update

--- .config/nvim/lua/leo/setting.lua
+++ .config/nvim/lua/leo/setting.lua
@@ -1,7 +1,12 @@
-vim.g.mapleader = ' '
-
+-- theme
 vim.opt.termguicolors = true -- Enable 24-bit RGB color in the TUI
-vim.opt.fileencoding = 'utf-8' -- File content encoding for the buffer
+vim.cmd.colorscheme("nightfly")
+vim.opt.background = "dark"
+
+-- setting
+vim.g.mapleader = " "
+
+vim.opt.fileencoding = "utf-8" -- File content encoding for the buffer
 vim.opt.hid = true
 vim.opt.wrap = false -- Disable wrapping of lines longer than the width of window
 -- vim.opt.iskeyword = 'UTF-8'
@@ -15,8 +20,8 @@ vim.opt.smarttab = true
 vim.opt.expandtab = true -- Enable the use of space in tab
 vim.opt.smartindent = true
 vim.opt.autoindent = true
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.formatoptions = 'cro'
+vim.opt.clipboard = "unnamedplus"
+vim.opt.formatoptions = "cro"
 vim.opt.updatetime = 300 -- Length of time to wait before triggering the plugin
 vim.opt.timeoutlen = 300 -- Length of time to wait for a mapped sequence
 vim.opt.ru = false -- ??
@@ -25,14 +30,14 @@ vim.opt.backup = false
 vim.opt.writebackup = false -- Disable making a backup before overwriting a file
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 -- vim.opt.swb = 'usopen'
 vim.opt.incsearch = true
 -- vim.opt.signcolumn = 'yes'
-vim.opt.completeopt = { 'menuone', 'noselect' }
+vim.opt.completeopt = { "menuone", "noselect" }
 vim.opt.history = 100 -- Number of commands to remember in a history table
-vim.opt.mouse = 'a' -- Enable mouse support
-vim.opt.signcolumn = 'yes' -- Always show the sign column
+vim.opt.mouse = "a" -- Enable mouse support
+vim.opt.signcolumn = "yes" -- Always show the sign column
 vim.opt.swapfile = false -- Disable use of swapfile for the buffer
 vim.opt.undofile = true -- Enable persistent undo
 vim.opt.backspace = vim.opt.backspace + { "nostop" } -- Don't stop backspace at insert
@@ -42,19 +47,14 @@ vim.opt.fillchars = { eob = " " } -- Disable `~` on nonexistent lines
 vim.opt.laststatus = 3 -- globalstatus
 vim.opt.lazyredraw = true -- lazily redraw screen
 vim.opt.preserveindent = true -- Preserve indent structure as much as possible
-vim.opt.pumheight = 10 -- Height of the pop up menu
+vim.opt.pumheight = 11 -- Height of the pop up menu
 vim.opt.scrolloff = 8 -- Number of lines to keep above and below the cursor
 vim.opt.sidescrolloff = 8 -- Number of columns to keep at the sides of the cursor
 
--- theme
-vim.g.tokyonight_transparent_sidebar = true
-vim.g.tokyonight_transparent = true
-vim.opt.background = 'dark'
-
 -- Highlight when yank
-vim.cmd[[
+vim.cmd([[
   augroup highlight_yank
     autocmd!
     au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=200}
   augroup END
-]]
+]])
