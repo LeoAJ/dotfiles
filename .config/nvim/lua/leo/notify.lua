caa9b9e4f683eab44c84a6c08ab7cfcc44849928
Leo Hsieh <leo0924@protonmail.com>
2022-11-10 22:29:17 +0800

    update

--- .config/nvim/lua/leo/notify.lua
+++ .config/nvim/lua/leo/notify.lua
@@ -1,8 +1,11 @@
 local status_ok, notify = pcall(require, "notify")
-if not status_ok then return end
+if not status_ok then
+	return
+end
 
-notify.setup {
-  stages = "fade"
-}
+notify.setup({
+	stages = "fade",
+	background_colour = "#000000",
+})
 
 vim.notify = notify
