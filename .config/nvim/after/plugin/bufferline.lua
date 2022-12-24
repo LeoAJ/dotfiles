d64b20f2986f8729bd45a1ae594d46709b3f68da
Leo Hsieh <leo0924@protonmail.com>
2022-12-24 17:58:39 +0800

    update lua config with after

--- .config/nvim/after/plugin/bufferline.lua
+++ .config/nvim/after/plugin/bufferline.lua
@@ -7,9 +7,6 @@ bufferline.setup({
 	options = {
 		show_close_icon = false,
 		show_buffer_close_icons = false,
-		-- buffer_close_icon = "",
-		modified_icon = "",
-		-- close_icon = "",
 		max_name_length = 14,
 		max_prefix_length = 13,
 		tab_size = 20,
--- .config/nvim/lua/leo/init.lua
+++ .config/nvim/lua/leo/init.lua
@@ -1,21 +1,3 @@
--- settings
 require("leo.setting")
 require("leo.mapping")
-
--- plugins
 require("leo.plugins")
-require("leo.Comment")
-require("leo.cmp")
-require("leo.lsp")
-require("leo.treesitter")
-require("leo.autopairs")
-require("leo.icons")
-require("leo.notify")
-require("leo.dressing")
-require("leo.telescope")
-require("leo.bufferline")
-require("leo.gitsigns")
-require("leo.better_escape")
-require("leo.indent-line")
-require("leo.toggleterm")
-require("leo.lualine")
