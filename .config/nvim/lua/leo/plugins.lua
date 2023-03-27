b44a32c2e47dfb89ababeab5dd296d8706e8019d
Leo Hsieh <leo0924@protonmail.com>
2023-03-26 23:58:33 -0700

    update

--- .config/nvim/lua/leo/plugins.lua
+++ .config/nvim/lua/leo/plugins.lua
@@ -74,26 +74,27 @@ return require("packer").startup(function(use)
 
 	-- LSP
   use {
-  'VonHeikemen/lsp-zero.nvim',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {'williamboman/mason.nvim'},           -- Optional
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-buffer'},
-    {'hrsh7th/cmp-path'},
-    {'saadparwaiz1/cmp_luasnip'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/cmp-nvim-lua'},
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},         -- Required
+      {'hrsh7th/cmp-nvim-lsp'},     -- Required
+      {'hrsh7th/cmp-buffer'},       -- Optional
+      {'hrsh7th/cmp-path'},         -- Optional
+      {'saadparwaiz1/cmp_luasnip'}, -- Optional
+      {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
-    -- Snippets
-    {'L3MON4D3/LuaSnip'},
-    -- {'rafamadriz/friendly-snippets'},
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},             -- Required
+      -- {'rafamadriz/friendly-snippets'}, -- Optional
+    }
   }
-}
 
 	-- tree sitter
 	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
