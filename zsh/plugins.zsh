5d53bfb60a4d5e830c720649b89ab9feec3b17fd
Leo Hsieh <leo0924@protonmail.com>
2023-04-13 23:57:14 -0700

    fix zsh

---
+++ zsh/plugins.zsh
@@ -0,0 +1,3 @@
+source ~/.config/zsh/plugins/zsh-autopair/autopair.zsh
+source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
