15f24a8aea6a0c04ba8ac225c5b1ceccf3a1697e
Leo Hsieh <leo0924@protonmail.com>
2023-11-05 09:06:24 -0800

    tmp save

--- .config/nvim/lua/leo/lazy.lua
+++ .config/nvim/lua/leo/lazy.lua
@@ -11,103 +11,15 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  "nvim-lua/plenary.nvim",
-
-  "tpope/vim-surround",
-
-  "romainl/vim-cool",
-
-  "lewis6991/impatient.nvim",
-
-  "p00f/nvim-ts-rainbow",
-
-  { "windwp/nvim-ts-autotag",          priority = 200 },
-
-  "windwp/nvim-autopairs",
-
-  "rcarriga/nvim-notify",
-
-  "stevearc/dressing.nvim",
-
-  {
-    "antoinemadec/FixCursorHold.nvim",
-    config = function()
-      vim.g.cursorhold_updatetime = 100
-    end
+require("lazy").setup({ { import = "leo.plugins" }, { import = "leo.plugins.lsp" } },{
+  install = {
+    colorscheme = { "catppuccin-mocha" },
   },
-
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    config = function()
-      vim.cmd("colorscheme catppuccin-mocha")
-    end,
-    priority = 1000
+  checker = {
+    enabled = true,
+    notify = false,
   },
-
-  "numToStr/Comment.nvim",
-
-  "honza/vim-snippets",
-
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    dependencies = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' }, -- Required
-      {
-        -- Optional
-        'williamboman/mason.nvim',
-        build = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
-      },
-      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },         -- Required
-      { 'hrsh7th/cmp-nvim-lsp' },     -- Required
-      { 'L3MON4D3/LuaSnip' },         -- Required
-      { 'hrsh7th/cmp-buffer' },       -- Optional
-      { 'hrsh7th/cmp-path' },         -- Optional
-      { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-      { 'hrsh7th/cmp-nvim-lua' },     -- Optional
-    }
+  change_detection = {
+    notify = false,
   },
-
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-
-  "nvim-treesitter/nvim-treesitter-context",
-
-  "JoosepAlviste/nvim-ts-context-commentstring",
-
-  "nvim-telescope/telescope.nvim",
-
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-
-  {
-    'akinsho/bufferline.nvim',
-    version = "v3.*",
-    dependencies =
-    'nvim-tree/nvim-web-devicons'
-  },
-
-  { 'jose-elias-alvarez/null-ls.nvim',          dependencies = 'nvim-lua/plenary.nvim' },
-
-  "lewis6991/gitsigns.nvim",
-
-  "max397574/better-escape.nvim",
-
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
-
-  "akinsho/toggleterm.nvim",
-
-  "christoomey/vim-tmux-navigator",
-
-  "nvim-lualine/lualine.nvim",
-
-  "simrat39/rust-tools.nvim",
-
-  "NvChad/nvterm"
 })
