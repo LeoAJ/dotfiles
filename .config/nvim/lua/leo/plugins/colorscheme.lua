72516e686cf6d52e491b344d034fa39e32977bd3
Leo Hsieh <leo0924@protonmail.com>
2023-12-06 23:29:58 -0800

    update

--- .config/nvim/lua/leo/plugins/colorscheme.lua
+++ .config/nvim/lua/leo/plugins/colorscheme.lua
@@ -1,9 +1,42 @@
+-- return {
+--   {
+--     "catppuccin/nvim",
+--     priority = 1000,
+--     config = function()
+--       vim.cmd("colorscheme catppuccin-mocha")
+--     end,
+--   }
+-- }
+
+-- return {
+-- 	{
+-- 		"ntk148v/habamax.nvim",
+-- 		dependencies = { "rktjmp/lush.nvim" },
+-- 		config = function()
+-- 			vim.cmd("colorscheme habamax")
+-- 		end,
+-- 	},
+-- }
+
 return {
-  {
-    "catppuccin/nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd("colorscheme catppuccin-mocha")
-    end,
-  }
+	{
+		"blazkowolf/gruber-darker.nvim",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			vim.cmd("colorscheme gruber-darker")
+		end,
+	},
 }
+
+-- return {
+-- 	{
+-- 		"neanias/everforest-nvim",
+-- 		version = false,
+-- 		lazy = false,
+-- 		priority = 1000, -- make sure to load this before all the other start plugins
+-- 		-- Optional; default configuration will be used if setup isn't called.
+-- 		config = function()
+-- 			vim.cmd("colorscheme everforest")
+-- 		end,
+-- 	},
+-- }
--- .config/nvim/lua/leo/plugins/lsp/lsp-zero.lua
+++ .config/nvim/lua/leo/plugins/lsp/lsp-zero.lua
@@ -32,14 +32,9 @@ return {
 		require("mason-lspconfig").setup({
 			ensure_installed = {
 				"tsserver",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"svelte",
 				"lua_ls",
 				"graphql",
 				"emmet_ls",
-				"prismals",
 				"pyright",
 			},
 			-- auto-install configured servers (with lspconfig)
