23bc1a4e01a616501bfa3e8ee9bd217f50366f21
Leo Hsieh <leo0924@protonmail.com>
2023-12-19 22:08:48 -0800

    update

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -19,7 +19,7 @@ export EDITOR=nvim
 
 # This is for `z` command
 # Move next only if `homebrew` is installed
-if command -v brew >/dev/null 2>&1; then
-	# Load rupa's z if installed
-	[ -f $(brew --prefix)/etc/profile.d/z.sh ] && source $(brew --prefix)/etc/profile.d/z.sh
-fi
+# if command -v brew >/dev/null 2>&1; then
+# 	# Load rupa's z if installed
+# 	[ -f $(brew --prefix)/etc/profile.d/z.sh ] && source $(brew --prefix)/etc/profile.d/z.sh
+# fi
