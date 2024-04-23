8e2569489a3fb6178f0fa63b5a32feb9e1d1b8fe
Leo Hsieh <leo0924@protonmail.com>
2024-04-22 21:17:26 -0700

    update

--- .config/nvim/lua/leo/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/leo/plugins/nvim-treesitter.lua
@@ -16,6 +16,7 @@ return {
 			treesitter.setup({ -- enable syntax highlighting
 				highlight = {
 					enable = true,
+					additional_vim_regex_highlighting = false,
 				},
 				-- enable indentation
 				indent = { enable = true },
