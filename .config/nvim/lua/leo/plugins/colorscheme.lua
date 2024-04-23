c151b9ae948d4f28dfad40d4beff3fb6754b066e
Leo Hsieh <leo0924@protonmail.com>
2024-04-22 21:17:26 -0700

    update

--- .config/nvim/lua/leo/plugins/colorscheme.lua
+++ .config/nvim/lua/leo/plugins/colorscheme.lua
@@ -1,12 +1,39 @@
 -- return {
---   {
---     "catppuccin/nvim",
---     priority = 1000,
---     config = function()
---       vim.cmd("colorscheme catppuccin-mocha")
---     end,
---   }
+-- 	"savq/melange-nvim",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.cmd("colorscheme melange")
+-- 	end,
 -- }
+
+-- return {
+-- 	"sainnhe/gruvbox-material",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.g.gruvbox_material_transparent_background = 0
+-- 		vim.g.gruvbox_material_foreground = "mix"
+-- 		vim.g.gruvbox_material_background = "hard" -- soft, medium, hard
+-- 		vim.g.gruvbox_material_ui_contrast = "high" -- The contrast of line numbers, indent lines, etc.
+-- 		vim.g.gruvbox_material_float_style = "bright" -- Background of floating windows
+-- 		vim.g.gruvbox_material_statusline_style = "material"
+-- 		vim.g.gruvbox_material_cursor = "auto"
+-- 		vim.cmd("colorscheme gruvbox-material")
+-- 	end,
+-- }
+
+return {
+	{
+		"catppuccin/nvim",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = false,
+				flavour = "mocha",
+			})
+			vim.cmd("colorscheme catppuccin")
+		end,
+	},
+}
 
 -- return {
 -- 	{
@@ -18,15 +45,29 @@
 -- 	},
 -- }
 
-return {
-	{
-		"blazkowolf/gruber-darker.nvim",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			vim.cmd("colorscheme gruber-darker")
-		end,
-	},
-}
+-- return {
+-- 	"rose-pine/neovim",
+-- 	priority = 1000, -- make sure to load this before all the other start plugins
+-- 	config = function()
+-- 		require("rose-pine").setup({
+-- 			-- disable_background = true,
+-- 			styles = {
+-- 				italic = false,
+-- 			},
+-- 		})
+-- 		vim.cmd("colorscheme rose-pine")
+-- 	end,
+-- }
+
+-- return {
+-- 	{
+-- 		"blazkowolf/gruber-darker.nvim",
+-- 		priority = 1000, -- make sure to load this before all the other start plugins
+-- 		config = function()
+-- 			vim.cmd("colorscheme gruber-darker")
+-- 		end,
+-- 	},
+-- }
 
 -- return {
 -- 	{
