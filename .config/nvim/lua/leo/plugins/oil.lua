7ae9daf659f0ae262b1de0e0b28352241a87cc55
Leo Hsieh <leo0924@protonmail.com>
2024-04-28 23:06:35 -0700

    clean up

---
+++ .config/nvim/lua/leo/plugins/oil.lua
@@ -0,0 +1,14 @@
+return {
+	"stevearc/oil.nvim",
+	opts = {},
+	-- Optional dependencies
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+		})
+		vim.keymap.set("n", "<leader>e", "<cmd>Oil<cr>", { desc = "Open parent directory" })
+	end,
+}
