32b6046b6c932b0cf371a677ed0de5b3fe98ebc7
Leo Hsieh <leo0924@protonmail.com>
2025-03-09 22:54:12 -0700

    update lsp

---
+++ .config/nvim/lua/leo/plugins/blink.lua
@@ -0,0 +1,26 @@
+return {
+	"saghen/blink.cmp",
+	dependencies = "rafamadriz/friendly-snippets",
+	version = "*",
+	opts = {
+		keymap = { preset = "default" },
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
+			nerd_font_variant = "normal",
+		},
+
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+
+		-- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
+		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+		--
+		-- See the fuzzy documentation for more information
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+	},
+	opts_extend = { "sources.default" },
+}
