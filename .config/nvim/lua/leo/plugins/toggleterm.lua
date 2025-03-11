412f6ed3bce37e2337a9a399349f1ccada016a28
Leo Hsieh <leo0924@protonmail.com>
2025-03-11 04:50:54 -0700

    term

--- .config/nvim/lua/leo/plugins/toggleterm.lua
+++ .config/nvim/lua/leo/plugins/toggleterm.lua
@@ -1,21 +1,21 @@
 return {
-	"akinsho/toggleterm.nvim",
-	config = function()
-		require("toggleterm").setup({
-			shell = vim.o.shell,
-			size = 80,
-			open_mapping = [[<c-\>]],
-			shading_factor = 2,
-			direction = "vertical",
-			-- float_opts = {
-			-- 	border = "curved",
-			-- 	highlights = {
-			-- 		border = "Normal",
-			-- 		background = "Normal",
-			-- 	},
-			-- },
-		})
-
-		vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<cr>")
-	end,
+	-- "akinsho/toggleterm.nvim",
+	-- config = function()
+	-- 	require("toggleterm").setup({
+	-- 		shell = vim.o.shell,
+	-- 		size = 80,
+	-- 		open_mapping = [[<c-\>]],
+	-- 		shading_factor = 2,
+	-- 		direction = "vertical",
+	-- 		-- float_opts = {
+	-- 		-- 	border = "curved",
+	-- 		-- 	highlights = {
+	-- 		-- 		border = "Normal",
+	-- 		-- 		background = "Normal",
+	-- 		-- 	},
+	-- 		-- },
+	-- 	})
+	--
+	-- 	vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<cr>")
+	-- end,
 }
