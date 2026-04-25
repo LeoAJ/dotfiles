a32791f3a7009fcc3bc24621e0dc44a08683254a
Leo Hsieh <leo0924@protonmail.com>
2026-04-24 23:03:32 -0700

    sync

--- .config/nvim/lua/leo/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/leo/plugins/nvim-treesitter.lua
@@ -3,57 +3,34 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		event = { "BufReadPre", "BufNewFile" },
 		build = ":TSUpdate",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-			"windwp/nvim-ts-autotag",
+		-- dependencies = {
+		-- 	"nvim-treesitter/nvim-treesitter-textobjects",
+		-- 	"windwp/nvim-ts-autotag",
+		-- },
+		opts = {
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+			ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"markdown",
+				"bash",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+				"python",
+			},
 		},
-		config = function()
-			vim.g.skip_ts_context_commentstring_module = true
-			-- import nvim-treesitter plugin
-			local treesitter = require("nvim-treesitter.configs")
-
-			-- configure treesitter
-			treesitter.setup({ -- enable syntax highlighting
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-				-- enable indentation
-				indent = { enable = true },
-				-- enable autotagging (w/ nvim-ts-autotag plugin)
-				autotag = {
-					enable = true,
-				},
-				-- ensure these language parsers are installed
-				ensure_installed = {
-					"json",
-					"javascript",
-					"typescript",
-					"tsx",
-					"yaml",
-					"html",
-					"css",
-					"markdown",
-					"graphql",
-					"bash",
-					"lua",
-					"vim",
-					"dockerfile",
-					"gitignore",
-					"python",
-					"rust",
-				},
-				incremental_selection = {
-					enable = true,
-					-- keymaps = {
-					-- 	init_selection = "<C-space>",
-					-- 	node_incremental = "<C-space>",
-					-- 	scope_incremental = false,
-					-- 	node_decremental = "<bs>",
-					-- },
-				},
-				enable_autocmd = false,
-			})
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
 		end,
 	},
 }
